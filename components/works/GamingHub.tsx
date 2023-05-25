fac23bc28495f2d18ad0480da4e8d7968e7a5bbc
unknown <marbella.tino@gmail.com>
2023-05-25 18:18:47 +0800

    fixed

--- components/works/GamingHub.tsx
+++ components/works/GamingHub.tsx
@@ -8,15 +8,14 @@ function GamingHub() {
       animate={{ opacity: 1 }}
       transition={{ delay: 0.1 }}
     >
-      <h3 className="flex gap-1 font-medium text-xl font-titleFont">
-        Owner
+      <h3 className="flex gap-1 font-medium text-xl font-titleFont">Owner
         <span className="text-textGreen tracking-wide">@GamingHub</span>
       </h3>
-      <p className="text-sm mt-1 font-medium text-textDark">July 2014 - January 2020</p>
+      <p className="text-sm mt-1 font-medium text-textDark">Julk 2014 - Jan 2020</p>
       <ul className="mt-6 flex flex-col gap-3">
         <li className="text-base flex gap-2 text-textDark">
           <span className="text-textGreen mt-1"><TiArrowForward /></span>
-          As a former owner of a computer shop, it's a great experience owning one. I had many players in the shop, and there was no age gap. We all just had fun playing games. I also managed a team that competed in League of Legends tournaments in our town.
+          As a former owner of a computer shop, It&rsquo;s a great experience owning one. I have many players in the shop. There ain&rsquo;t no age gap; we all just have fun playing games. I also manage a team that competes in League of Legends Tournaments here in our town.
         </li>
       </ul>
     </motion.div>
