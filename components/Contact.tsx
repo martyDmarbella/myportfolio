38ef197b47dea9ec577f765a8fd60661e2ed8f83
unknown <marbella.tino@gmail.com>
2023-05-25 18:24:56 +0800

    fix

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <section id='contact' className='max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4 items-center justify-center'>
       <p className='font-titleFont text-lg text-textGreen font-semibold flex items-center tracking-wide'>
-        04 What&apos;s Next?
+        04 What&apos;s Next??
       </p>
       <h2 className='font-titleFont text-5xl font-semibold'>
         Get In Touch
